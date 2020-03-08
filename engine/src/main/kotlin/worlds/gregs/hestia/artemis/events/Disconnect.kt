6456f3c76ef6110a92798691d4439e4ceafd3a43
GregHib <gregoryhib@gmail.com>
2020-03-08 16:12:46 +0000

    Disconnect all clients to lobby on shutdown hook, closes #142

--- engine/src/main/kotlin/worlds/gregs/hestia/artemis/events/Disconnect.kt
+++ engine/src/main/kotlin/worlds/gregs/hestia/artemis/events/Disconnect.kt
@@ -2,7 +2,8 @@ package worlds.gregs.hestia.artemis.events
 
 import com.artemis.Entity
 import net.mostlyoriginal.api.event.common.Event
+import worlds.gregs.hestia.artemis.InstantEvent
 
-class Disconnect(val entityId: Int) : Event {
+class Disconnect(val entityId: Int) : Event, InstantEvent {
     constructor(entity: Entity) : this(entity.id)
 }
