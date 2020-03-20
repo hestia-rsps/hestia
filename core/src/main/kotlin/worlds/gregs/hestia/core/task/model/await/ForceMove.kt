001255a1d288c037389bb0bc52fc591de7bb912b
GregHib <gregoryhib@gmail.com>
2020-03-20 20:19:11 +0000

    WIP

---
+++ core/src/main/kotlin/worlds/gregs/hestia/core/task/model/await/ForceMove.kt
@@ -0,0 +1,9 @@
+package worlds.gregs.hestia.core.task.model.await
+
+import kotlinx.coroutines.CancellableContinuation
+import worlds.gregs.hestia.core.display.interfaces.model.Window
+import worlds.gregs.hestia.core.task.api.TaskSuspension
+
+data class ForceMove(val window: Window = Window.MAIN_SCREEN) : TaskSuspension<Unit> {
+    override lateinit var continuation: CancellableContinuation<Unit>
+}
